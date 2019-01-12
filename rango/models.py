cb968ea67c5fdb960d6e2436c8727962745c6945
ZAIN PC <m.zain.ul.islam@gmail.com>
2019-01-12 19:14:45 +0000

    Set UP DB and Models

--- rango/models.py
+++ rango/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
+
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):  # Just like toString() in Java
+        return self.name
+
+    def __unicode__(self):  # For Unicode Support, Python 2.x
+        return self.name
+
+
+class Page(models.Model):
+    category = models.ForeignKey(Category)
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
+    def __unicode__(self):  # For Unicode Support, Python 2.x
+        return self.title
